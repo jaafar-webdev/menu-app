9f95c9504aa702c1a648f096ca18df37f7c4a176
GaaferAlwakeil <venditathisme@gmail.com>
2025-08-15 17:51:07 +0300

    Remove unused dashboard components and related files

--- src/app/(dashboard)/addproduct/page.js
+++
@@ -1,9 +0,0 @@
-export default function AddProductPage() {
-  return (
-    <div>
-      <h1 className="text-2xl font-bold">Add New Product</h1>
-      <p>This is where you can add a new product.</p>
-      {/* Add your product form here */}
-    </div>
-  );
-}
--- src/app/(dashboard)/products/page.js
+++
@@ -1,9 +0,0 @@
-export default function ProductsPage() {
-  return (
-    <div>
-      <h1 className="text-2xl font-bold">View Products</h1>
-      <p>This page will display all products.</p>
-      {/* Add your products list here */}
-    </div>
-  );
-}
